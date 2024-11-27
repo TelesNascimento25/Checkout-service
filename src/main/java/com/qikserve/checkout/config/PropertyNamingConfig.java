9f283a2c4800ba27550acdf5991a15a824eca258
Darlysson Teles <123102867+TelesNascimento25@users.noreply.github.com>
2024-11-27 13:29:46 -0300

    Added utils

---
+++ src/main/java/com/qikserve/checkout/config/PropertyNamingConfig.java
@@ -0,0 +1,16 @@
+package com.qikserve.checkout.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import io.swagger.v3.core.jackson.ModelResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PropertyNamingConfig {
+
+    @Bean
+    public ModelResolver modelResolver(ObjectMapper objectMapper) {
+        return new ModelResolver(objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE));
+    }
+}
